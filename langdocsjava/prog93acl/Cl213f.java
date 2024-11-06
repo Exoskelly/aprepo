e3d39ecc2d0e7dbff6872424097740d0ea1b107f
Exoskelly <150711442+Exoskelly@users.noreply.github.com>
2024-11-06 17:24:31 +0000

    one lined

---
+++ langdocsjava/prog93acl/Cl213f.java
@@ -0,0 +1,11 @@
+package prog93acl;
+
+public class Cl213f {
+    private double myKmh;
+    private double myCost;
+
+    public Cl213f(double kmh) {
+        myKmh = kmh;
+        myCost = 0;
+    }
+}
